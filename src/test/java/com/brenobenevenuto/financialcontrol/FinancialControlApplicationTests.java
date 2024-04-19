43777803c727cd378077def4bcc24109e7031e4b
Breno Benevenuto <brenobenevenuto@gmail.com>
2024-04-18 23:45:31 -0300

    Criação Projeto

---
+++ src/test/java/com/brenobenevenuto/financialcontrol/FinancialControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brenobenevenuto.financialcontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+class FinancialControlApplicationTests {
+
+
+	void contextLoads() {
+	}
+
+}
