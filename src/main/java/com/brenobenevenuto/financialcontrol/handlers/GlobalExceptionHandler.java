6a12b4539a312ed4a3b6dc2e44039c13984af3ea
unknown <pedroluis20202@gmail.com>
2024-04-26 00:58:26 -0300

    [FEAT] Implementing global exception handler

---
+++ src/main/java/com/brenobenevenuto/financialcontrol/handlers/GlobalExceptionHandler.java
@@ -0,0 +1,16 @@
+package com.brenobenevenuto.financialcontrol.handlers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.brenobenevenuto.financialcontrol.exceptions.UserNotFoundException;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<String>HandleUserNotFound(Exception exception){
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+    }
+}
